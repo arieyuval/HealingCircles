5f588501bae935d2c1a0a4293aeb16fa9a017fc2
arieyuval <arieyuval33@gmail.com>
2025-07-01 22:25:11 -0700

    enabled analytics

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/next"
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
